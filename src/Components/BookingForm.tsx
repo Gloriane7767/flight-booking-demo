a71cc1f72bd658d8ca960f84961bb06c151b0a6c
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-05-04 12:24:15 +0200

    refactor BookingForm component structure and update import path

---
+++ src/Components/BookingForm.tsx
@@ -0,0 +1,38 @@
+
+const BookingForm = () => {
+    return (
+        <div className="bg-gray-100 p-6">
+            <h1 className="text-3xl font-bold mb-6">✈️ Flight Reservation</h1>
+
+            {/* FILTER */}
+            <div className="mb-4 space-x-4">
+
+                {/* All Flights */}
+
+                {/* Available Flights */}
+
+            </div>
+
+            {/* FLIGHTS */}
+            <div id="flights" className="space-y-3 mb-10"></div>
+
+            {/* BOOKING FORM */}
+
+
+            {/* SEARCH */}
+            <div className="bg-white p-4 rounded shadow mb-6">
+
+            </div>
+
+            {/* BOOKINGS */}
+            <div id="bookings" className="space-y-3"></div>
+
+            {/* CANCELLATION */}
+            <div className="bg-white p-4 rounded shadow mb-6">
+
+            </div>
+        </div>
+    );
+};
+
+export default BookingForm;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import BookingForm from './BookingForm.tsx'
+import BookingForm from './Components/BookingForm.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
